1c3120c786bf1cbb4428fc44cf3f2a243f03d213
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-24 15:25:39 +0200

    Ex 2

---
+++ Trucks/DataProcessor/ImportDto/ImportJsonClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trucks.DataProcessor.ImportDto
+{
+    public class ImportJsonClient
+    {
+        public string Name { get; set; }
+    }
+}
