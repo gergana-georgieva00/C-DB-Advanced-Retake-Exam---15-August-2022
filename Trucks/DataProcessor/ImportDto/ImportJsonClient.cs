f4eb399fbda110652bc5e8f60230115fe3443b2b
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-24 15:29:15 +0200

    Ex 2

--- Trucks/DataProcessor/ImportDto/ImportJsonClient.cs
+++ Trucks/DataProcessor/ImportDto/ImportJsonClient.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Trucks.Data.Models;
 
 namespace Trucks.DataProcessor.ImportDto
 {
     public class ImportJsonClient
     {
-        public string Name { get; set; }
+        [Required, MinLength(3), MaxLength(40)]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Nationality { get; set; }
+
+        public string? Type { get; set; }
+        public ICollection<ClientTruck> ClientsTrucks { get; set; }
     }
 }
