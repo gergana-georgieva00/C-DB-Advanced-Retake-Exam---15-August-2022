79062bc9c3ebc636fae5f3cbc7bfd28a81666882
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-25 22:29:39 +0200

    Ex 3

--- Trucks/DataProcessor/Serializer.cs
+++ Trucks/DataProcessor/Serializer.cs
@@ -1,6 +1,8 @@
 ﻿namespace Trucks.DataProcessor
 {
     using Data;
+    using Newtonsoft.Json;
+    using Trucks.Data.Models.Enums;
 
     public class Serializer
     {
@@ -11,7 +13,33 @@
 
         public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
         {
-            throw new NotImplementedException();
+            var clients = context.Clients
+                .Where(c => c.ClientsTrucks.Any(ct => ct.Truck.TankCapacity >= capacity))
+                .ToArray()
+                .Select(c => new
+                {
+                    Name = c.Name,
+                    Trucks = c.ClientsTrucks
+                    .Where(ct => ct.Truck.TankCapacity >= capacity)
+                    .Select(ct => new
+                    {
+                        TruckRegistrationNumber = ct.Truck.RegistrationNumber,
+                        VinNumber = ct.Truck.VinNumber,
+                        TankCapacity = ct.Truck.TankCapacity,
+                        CargoCapacity = ct.Truck.CargoCapacity,
+                        CategoryType = ct.Truck.CategoryType.ToString(),
+                        MakeType = ct.Truck.MakeType.ToString()
+                    })
+                    .OrderBy(ct => ct.MakeType.ToString())
+                    .ThenByDescending(ct => ct.CargoCapacity.ToString())
+                    .ToArray()
+                })
+                .OrderByDescending(c => c.Trucks.Count()).
+                ThenBy(c => c.Name)
+                .Take(10)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(clients, Formatting.Indented);    
         }
     }
 }
