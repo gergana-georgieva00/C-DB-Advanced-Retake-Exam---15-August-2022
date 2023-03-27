c918223ea4e7ea0f8ddad88f7b8d80b0bb5f4f8b
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-27 12:02:01 +0300

    Ex 2

--- Trucks/DataProcessor/ImportDto/ImportXmlDespatcher.cs
+++ Trucks/DataProcessor/ImportDto/ImportXmlDespatcher.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Trucks.Data.Models;
 
 namespace Trucks.DataProcessor.ImportDto
 {
+    [XmlType("Despatcher")]
     public class ImportXmlDespatcher
     {
+        [XmlElement("Name")]
+        [Required,MinLength(2), MaxLength(40)]
+        public string? Name { get; set; }
+
+        public string? Position { get; set; }
+        public ICollection<Truck> Trucks { get; set; }
     }
 }
