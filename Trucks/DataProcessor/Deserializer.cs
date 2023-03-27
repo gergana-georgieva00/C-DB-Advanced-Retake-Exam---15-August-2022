150296ba5999e801c761c3510538435c940725d1
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-27 14:12:48 +0300

    Exercise 2

--- Trucks/DataProcessor/Deserializer.cs
+++ Trucks/DataProcessor/Deserializer.cs
@@ -28,6 +28,11 @@
         {
             var root = new XmlRootAttribute(rootName);
             var serializer = new XmlSerializer(typeof(T), root);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            T dtos = (T)serializer.Deserialize(reader);
+            return dtos;
         }
 
         public static string ImportClient(TrucksContext context, string jsonString)
