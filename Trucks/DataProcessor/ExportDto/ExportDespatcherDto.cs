377ff09c83cb91b7fcd4c9b355910fc330ab1b61
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-27 14:34:47 +0300

    Ex 3

---
+++ Trucks/DataProcessor/ExportDto/ExportDespatcherDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ExportDto
+{
+    [XmlType("Despatcher")]
+    public class ExportDespatcherDto
+    {
+        public int TrucksCount { get; set; }
+        public string DespatcherName { get; set; }
+        public ExportTruckDto[] Trucks { get; set; }
+    }
+
+    public class ExportTruckDto
+    {
+        public string RegistrationNumber { get; set; }
+        public string Make { get; set; }
+    }
+}
